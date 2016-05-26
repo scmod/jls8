f6bc4a1305c57119ba1bcd997ea91c33259a6b12
hehe <ppp@ppp.com>
2016-05-26 11:18:48 +0800

    大概的学习下jls8..

---
+++ src/main/java/cc/TypeVariable.java
@@ -0,0 +1,57 @@
+package cc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TypeVariable {
+
+	
+	<T extends V1 & V2> void method(T t) {
+		t.methodV1();
+		t.methodV2();
+	}
+	
+	<T extends V1, U extends V2> void method(T t, U u) {
+		t.methodV1();
+		u.methodV1();
+		u.methodV2();
+	}
+	
+	static <K, V> Map<K, V> newHashMap() {
+		return new HashMap<K, V>();
+	}
+	
+	public static void main(String[] args) {
+		//<String, String>newHashMap();		//报错
+		TypeVariable.<String, String>newHashMap();//x.<>就没事...
+		setMap(TypeVariable.<String, String>newHashMap());//略神奇
+	}
+	
+	static void setMap(Map<String, String> map) {
+		
+	}
+	
+}
+
+class V1 {
+//	void methodV1() {
+//		
+//	}
+	
+	public void methodV1() {
+		System.out.println("V1 V1");
+	}
+}
+
+interface V2 {
+	void methodV1();
+	void methodV2();
+}
+
+class Vx extends V1 implements V2 {
+
+	public void methodV2() {
+		
+	}
+	
+}
